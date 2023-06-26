4395add7e8ec16c837be6da478a6e4e833383568
Marhosius <marhosius@gmail.com>
2023-06-27 01:16:09 +0300

    api

---
+++ src/api/authorization.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+const instance = axios.create({
+    baseURL: 'https://connections-api.herokuapp.com/',
+})
+
+export const setToken = (token) => {
+    instance.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
+
+export const dellToken = () => {
+    delete instance.defaults.headers.common['Authorization']
+}
+
+export const signUp = async (body) => {
+    return await instance.post('/users/signup', body)
+}
+
+export const logIn = async (body) => {
+    const { data } = await instance.post('/users/login', body)
+    if ('access_token' in data) setToken(data.access_token)
+    return data
+}
+
+export const getProfile = async () => {
+    const { data } = await instance('/users/current')
+    return data
+}
+
